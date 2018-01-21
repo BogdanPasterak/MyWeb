c6454690bc6d4e1ab1aa09cc0c701e7c7bfb6f44
BogdanPasterak <bogdanpasterak@gmail.com>
2018-01-21 01:00:53 +0000

    fix: position nav bar

--- jqscript.js
+++ jqscript.js
@@ -1,3 +1,26 @@
+window.onload = function() {
+
+	window.addEventListener("resize", myResize);
+	myResize();
+
+
+}
+
+const myResize = () => {
+
+	const width = document.documentElement.clientWidth;
+	//console.log(width);
+	if (width > 1270) {
+		const offset = ((width - 1270) / 2) + 'px';
+		document.getElementsByClassName("nav_bar")[0].style = 'left: ' +  offset;
+		document.getElementById("clock").style = 'right: ' + offset;
+	} else {
+		document.getElementsByClassName("nav_bar")[0].removeAttribute("style");
+		document.getElementById("clock").removeAttribute("style");
+	}
+
+}
+
 const hamburger = () => {
     const x = document.getElementsByClassName("nav_bar")[0];
     if (x.className === "nav_bar") {
