67772668abedaeb3f0b08dfe7a6077373030ca39
BogdanPasterak <bogdanpasterak@gmail.com>
2018-01-28 17:35:28 +0000

    fead: Google-EMEA

--- jqscript.js
+++ jqscript.js
@@ -10,14 +10,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	window.addEventListener("resize", myResize);
 	myResize();
 
-	initCanvas();
 
-	snake = new Snake();
+	if (document.getElementById('container').contains(document.getElementById('game'))) {
+
+		initCanvas();
+
+		snake = new Snake();
 
 
-	snake.start(50);
-
-
+		snake.start(30);
+	
+	}
 	//initGame();
 
 });
@@ -54,6 +57,7 @@ const clock = () => {
 const initCanvas = () => {
 
 	const c = document.getElementById("canvas");
+
 	const g = document.getElementById("game").getBoundingClientRect();
 
 	c.width = (g.width & 0xFFFE) - 10 ;
@@ -77,140 +81,17 @@ const initCanvas = () => {
 	
 	point.setXY(9, 1);
 	point.setColor('rgb(255, 0, 0)');
-	point.setXY(10, 1);
+	point.setXY(11, 4);
 	point.setColor('rgb(0, 255, 0)');
-	point.setXY(11, 1);
-	point.setColor('rgb(0, 0, 255)');
+	point.setXY(11, 5);
+	point.setColor('rgb(0, 255, 0)');
+	point.setXY(12, 6);
+	point.setColor('rgb(0, 255, 0)');
+	point.setXY(14, 8);
+	point.setColor('rgb(0, 255, 0)');
+	point.setXY(16, 10);
+	point.setColor('rgb(0, 255, 0)');
+
 
 };
 
-const drawSnake = () => {
-	// move head
-	position(snake[0]);
-	
-	// next step
-	change = whereNext();
-	way = (way + change) % 8;
-
-	//console.log('licznik = ' + step);
-
-	// 
-
-
-
-	// grow
-	if (step % 50 == 0) {
-		snake.push({x: snake[snake.length - 1].x, y: snake[snake.length - 1].y});
-	}
-	
-};
-
-const whereNext = () => {
-
-	//let see = look(ctx, snake[0], 0);
-	//console.log(see);
-
-	let next = {x: 0, y: 0};
-	copy(snake[0], next);
-
-	let mem = {x: 0, y: 0};
-	copy(next, mem);
-
-
-	if (way & 1 == 0) {
-		position(next);
-		if ( getPixel(next.x, next.y, ctx) == 'rgb(255, 0, 0)') {
-			console.log('przeszkoda-HW  step = ' + step + '  way=' + way);
-			if (Math.random() * 2 < 1) return 1;
-			else return 7;
-		}
-		copy(mem, next);
-		posR(next);
-		if ( getPixel(next.x, next.y, ctx) == 'rgb(255, 0, 0)') {
-			console.log('w lewo  step = ' + step + '  way=' + way);
-			return 7;
-		}
-		copy(mem, next);
-		posL(next);
-		if ( getPixel(next.x, next.y, ctx) == 'rgb(255, 0, 0)') {
-			console.log('w prawo  step = ' + step + '  way=' + way);
-			return 1;
-		}
-		copy(mem, next);
-		position(next);
-		position(next);
-		if ( getPixel(next.x, next.y, ctx) == 'rgb(255, 0, 0)') {
-			console.log('przeszkoda-HW-far  step = ' + step + '  way=' + way);
-			if (Math.random() * 2 < 1) return 1;
-			else return 7;
-		}
-	} else {
-		posR(next);
-		if ( getPixel(next.x, next.y, ctx) == 'rgb(255, 0, 0)') {
-			console.log('w lewo  step = ' + step + '  way=' + way);
-			return 7;
-		}
-		copy(mem, next);
-		posL(next);
-		if ( getPixel(next.x, next.y, ctx) == 'rgb(255, 0, 0)') {
-			console.log('w prawo  step = ' + step + '  way=' + way);
-			return 1;
-		}
-		copy(mem, next);
-		position(next);
-		if ( getPixel(next.x, next.y, ctx) == 'rgb(255, 0, 0)') {
-			console.log('przeszkoda-losuje  step = ' + step + '  way=' + way);
-			if (Math.random() * 2 < 1) return 1;
-			else return 7;
-		}
-	}
-
-
-	const rnd = (Math.random() * 51) | 0;
-	if (rnd == 0) {console.log('W prawo  step = ' + step); return 1;}
-	else if (rnd == 1) {console.log('W lewo step = ' + step); return 7;}
-	return 0;
-};
-
-
-
-const setPixel = (x, y, ctx) => {
-	ctx.fillRect(x << 1, y << 1, 2, 2);
-};
-
-const getPixel = (x, y, ctx) => {
-	const data = ctx.getImageData(x << 1, y << 1, 1, 1);
-	let s = 'rgb(';
-	s += data.data[0] + ', ';
-	s += data.data[1] + ', ';
-	s += data.data[2] + ')';
-	return s;
-};
-
-const position = (p) => {
-	if (way == 0 || way == 7 || way ==1) { p.x++; }
-	if (way == 4 || way == 3 || way ==5) { p.x--; }
-	if (way == 2 || way == 1 || way ==3) { p.y++; }
-	if (way == 6 || way == 5 || way ==7) { p.y--; }
-};
-
-const posR = (p) => {
-	let w = (way + 1) % 8;
-	if (w == 0 || w == 7 || w ==1) { p.x++; }
-	if (w == 4 || w == 3 || w ==5) { p.x--; }
-	if (w == 2 || w == 1 || w ==3) { p.y++; }
-	if (w == 6 || w == 5 || w ==7) { p.y--; }
-};
-
-const posL = (p) => {
-	let w = (way + 7) % 8;
-	if (w == 0 || w == 7 || w ==1) { p.x++; }
-	if (w == 4 || w == 3 || w ==5) { p.x--; }
-	if (w == 2 || w == 1 || w ==3) { p.y++; }
-	if (w == 6 || w == 5 || w ==7) { p.y--; }
-};
-
-const copy = (from,to) => {
-	to.x = from.x;
-	to.y = from.y;
-};
--- myLib.js
+++ myLib.js
@@ -1,9 +1,14 @@
 
 
 // TODO: A point object and a chain of function prototypes for it
-const Point = function () {
-  this.x = 0;
-  this.y = 0;
+const Point = function (point) {
+  if (point == undefined) {
+    this.x = 0;
+    this.y = 0;
+  } else {
+    this.x = point.x;
+    this.y = point.y;
+  }
   Point.ctx;  // Static variable
   // Static method
   Point.setCTX = function(ctx) {
@@ -83,12 +88,11 @@ const Snake = function() {
   this.body = [new Point(), new Point(), new Point()];
   this.head = this.body[0];
   this.eye = new Point();
-  this.eyeL = new Point();
-  this.eyeR = new Point();
   this.way = 0;
   this.speed = 0;
   this.step = 0;
   this.interval;
+  this.sees = {f: '', l: '', r: '', ff: '', fff: ''};
   Snake.see = {
     head: 'rgb(0, 0, 0)',
     body: 'rgb(128, 128, 128)',
@@ -129,13 +133,12 @@ Snake.prototype.start = function(speed) {
 Snake.prototype.stop = function(speed) {
   clearInterval(this.interval);
   this.interval = undefined;
+  console.log("Snake stop");
 };
 
 Snake.prototype.draw = function() {
 
   this.step++;
-
-  if (this.step > 500) this.stop();
 
   // erase till
   this.body[this.body.length-1].setColor('rgb(255, 255, 255)');
@@ -144,8 +147,8 @@ Snake.prototype.draw = function() {
   if (this.step % 100 == 0){
     this.body.pop();
     if (this.body.length < 1){
-      this.stop();
       console.log("Snake died of hunger");
+      this.stop();
     } else {
       this.body[this.body.length-1].setColor('rgb(255, 255, 255)');
     }
@@ -160,9 +163,19 @@ Snake.prototype.draw = function() {
 
   this.where();
 
+  this.sees.f = this.head.getColor();
 
-  if (this.head.getColor() != Snake.see.none) {
-    console.log(this.head.getColor());
+  // step on ...
+  if (this.sees.f != Snake.see.none) {
+    if (this.sees.f == Snake.see.food) {
+      console.log('mniam  rosne');
+      this.body.push(new Point(this.body[this.body.length-1]));
+    } else if (this.sees.f == Snake.see.hurdle) {
+      console.log("Snake died on hurdle");
+      this.stop();
+    } else {
+      console.log(this.head.getColor());
+    }
   }
 
 
@@ -175,26 +188,75 @@ Snake.prototype.draw = function() {
 };
 
 Snake.prototype.where = function() {
-  this.eye.copy(this.head);
-  this.eyeL.copy(this.head);
-  this.eyeR.copy(this.head);
-  this.front(this.eye);
-  this.left(this.eyeL);
-  this.right(this.eyeR);
 
-  if (this.eye.getColor() == Snake.see.hurdle) {
-    console.log('zobaczył przed');
-    if (this.eyeL.getColor() == Snake.see.hurdle) {
-      console.log('zobaczył z lewej  way = ' + this.way);
+  this.eye.copy(this.head);
+  this.front(this.eye);
+  this.sees.f = this.eye.getColor();
+  this.front(this.eye);
+  this.sees.ff = this.eye.getColor();
+  this.front(this.eye);
+  this.sees.fff = this.eye.getColor();
+
+  this.eye.copy(this.head);
+  this.left(this.eye);
+  this.sees.l = this.eye.getColor();
+  this.front(this.eye);
+  this.sees.fl = this.eye.getColor();
+
+  this.eye.copy(this.head);
+  this.right(this.eye);
+  this.sees.r = this.eye.getColor();
+  this.front(this.eye);
+  this.sees.fr = this.eye.getColor();
+
+
+  if (this.sees.f == Snake.see.hurdle) {
+    console.log('zobaczył przeszkode przed');
+    if (this.sees.l == Snake.see.hurdle) {
       this.way = ++this.way & 7;
-      console.log('zobaczył z lewej  way = ' + this.way);
-    } else if (this.eyeR.getColor() == Snake.see.hurdle) {
-      console.log('zobaczył z prawej');
+    } else if (this.sees.r == Snake.see.hurdle) {
       this.way = --this.way & 7;
     }
-
-
     // w prawo lub lewo
+  } else if (this.sees.f == Snake.see.food) {
+    console.log('jedzenie !');
+  } else if (this.sees.f == Snake.see.none) {
+    if (this.sees.l == Snake.see.food) {
+      console.log('jedzenie po lewej !');
+      this.way = --this.way & 7;
+    } else if (this.sees.r == Snake.see.food) {
+    console.log('jedzenie po prawej!');
+      this.way = ++this.way & 7;
+    } else if (this.sees.ff == Snake.see.hurdle) {
+      console.log('daleko przeszkoda !!!');
+      if (this.sees.l == Snake.see.hurdle) {
+        console.log('jade w prawo !');
+        this.way = ++this.way & 7;
+      } else if (this.sees.r == Snake.see.hurdle) {
+        console.log('jade w lewo !');
+          this.way = --this.way & 7;
+      } else {
+        console.log('losuje skret');
+        if (Math.random() * 2 <1)
+          this.way = ++this.way & 7;
+        else
+          this.way = --this.way & 7;
+      }
+    } else if (this.sees.fff == Snake.see.hurdle) {
+      if (this.sees.l == Snake.see.hurdle) {
+        console.log('jade w prawo !');
+        this.way = ++this.way & 7;
+      } else if (this.sees.r == Snake.see.hurdle) {
+        console.log('jade w lewo !');
+          this.way = --this.way & 7;
+      } else {      
+       console.log('losuje skret bo fff przeszkoda');
+       if (Math.random() * 2 <1)
+         this.way = ++this.way & 7;
+       else
+         this.way = --this.way & 7;
+      }
+    }
   }
 
   this.front(this.head);
